3d706a1cd72a6dc7bfb2861ce229a6ff750fd163
ccases <87296410+ccases@users.noreply.github.com>
2021-09-03 19:53:55 +0800

    added features: mark placement and move history

--- tictactoe/assets/script.js
+++ tictactoe/assets/script.js
@@ -1,81 +1,79 @@
-function Player(legalMoves, turn, moveHistory=[]){
-    this.legalMoves = legalMoves;
-    this.turn = turn;
-    this.moveHistory = moveHistory;
-    this.score = 0;
-
-    this.showMoveHistory=function(){
-        console.log(this.moveHistory);
-    };
-    this.addMoveHistory = function(move){
-        this.moveHistory.push(move);
-    }
-    this.clearMoveHistory = function(){
-        this.moveHistory = [];
-    }
-}
+let p1turn = true,
+  p2turn = false;
+let p1score = 0,
+  p2score = 0;
 
 let squares = document.querySelectorAll(".square");
-// for(let i = 0; i<3; i++){
-//     for(let j = 0; j<3; j++){
-//         squares[i*3+j].setAttribute("id", "c"+i+"r"+j);
-//         console.log(squares[i*3+j].id);
-//     }
-// }
+let moveHistory = [];
 
-
-let p1 = new Player(squares, true);     // P1 GETS X
-let p2 = new Player(squares, false);    // P2 GETS O
-
-function onHover(){
-
-}
-
-console.log("Square[2] id: " + squares[2].id);
-
-
-for(let i = 0; i < squares.length; i++){
-    squares[i].setAttribute("id",i);
-    squares[i].addEventListener("mouseover", function(){
-        if(p1.turn){
-            this.classList.add("p1-hover");
-        }
-        else{
-            this.classList.add("p2-hover");
-        }
-    });
-    squares[i].addEventListener("mouseleave", function(){
-        if(p1.turn){
-            this.classList.remove("p1-hover");
-        }
-        else{
-            this.classList.remove("p2-hover");
-        }
-    });
-    squares[i].addEventListener("click", function(){
-        if(p1.turn){
-            p1.turn = false;
-            p2.turn = true;
-            this.classList.add("p1-marker");
-            p1.addMoveHistory(squares[i].id);
-        }
-        else{
-            p1.turn = true;
-            p2.turn=false;
-            this.classList.add("p2-marker");
-            p2.addMoveHistory(squares[i].id);
-        }
-        console.log(i);
-        p1.showMoveHistory();
-        p2.showMoveHistory();
-        
-        console.log("Square[2] id: " + squares[2].id);
-    });
-}
-console.log("Square[2] id: " + squares[2].id);
-function placeMark(){
-    if(p1.turn){
-
+for (let i = 0; i < squares.length; i++) {
+  squares[i].addEventListener("mouseover", function () {
+    if (p1turn) {
+      this.classList.add("p1-hover");
+    } else {
+      this.classList.add("p2-hover");
     }
-    else{}
+  });
+
+  squares[i].addEventListener("mouseleave", function () {
+    if (p1turn) {
+      this.classList.remove("p1-hover");
+    } else {
+      this.classList.remove("p2-hover");
+    }
+  });
+
+  squares[i].addEventListener("click", function () {
+    let arr = copyPreviousState();
+    if (p1turn) {
+      p1turn = false;
+      p2turn = true;
+      this.classList.add("p1-marker");
+
+      placeMarkOn(this, "X");
+    } else {
+      p1turn = true;
+      p2turn = false;
+      this.classList.add("p2-marker");
+
+      placeMarkOn(this, "O");
+    }
+    console.log(moveHistory);
+  });
+}
+
+function copyPreviousState() {
+  // to instantiate new arrays every time
+  let moveNum = moveHistory.length;
+  let arr = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  if (moveNum == 0) {
+    return arr;
+  }
+
+  for (let i = 0; i < moveHistory[0].length; i++) {
+    for (let j = 0; j < moveHistory[0][0].length; j++) {
+      arr[i][j] = moveHistory[moveNum - 1][i][j];
+    }
+  }
+  return arr;
+}
+
+function placeMarkOn(div, mark) {
+  // places and stores to move history
+  let idx = getIndices(div);
+  let arr = copyPreviousState();
+  arr[idx[0]][idx[1]] = mark;
+  moveHistory.push(arr);
+}
+
+function getIndices(div) {
+  let result = [0, 0];
+  result[0] = +div.id.substring(1, 2); //parsing row
+  result[1] = +div.id.substring(2, 3); //parsing col
+  return result;
 }
