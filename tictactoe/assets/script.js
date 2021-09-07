3a58a66680171ccbed60f66de497469df26f6d70
ccases <87296410+ccases@users.noreply.github.com>
2021-09-07 12:07:33 +0800

    added features: victory animation, msg, music, and modified movehistory navigation

--- tictactoe/assets/script.js
+++ tictactoe/assets/script.js
@@ -4,7 +4,17 @@ let p1turn = true,
   p2turn = false;
 let p1score = 0,
   p2score = 0;
-// let typingComplete = true;
+
+let images = [];
+function preloadImages() {
+  for (let x of arguments) {
+    let img = new Image();
+    img.src = x;
+    images.push(img);
+  }
+}
+
+preloadImages("assets/music-normal.svg", "assets/music-muted.svg");
 
 let mainBody = document.getElementsByTagName("body");
 let squares = document.querySelectorAll(".square");
@@ -16,23 +26,28 @@ let p1TurnPrompt = "It's Player 1's turn. (X)";
 let p2TurnPrompt = "It's Player 2's turn. (O)";
 
 let sfx = document.getElementsByClassName("sfx");
-// [0] = a sfx
-let music = document.getElementsByClassName("music");
+for (let s of sfx) {
+  s.muted = true;
+}
+
+let musics = document.getElementsByClassName("music");
+for (let m of musics) {
+  m.muted = true;
+}
+
 let musicVol = document.getElementById("music-vol");
 musicVol.addEventListener("click", (e) => {
   console.log("clicked music");
   let img = document.getElementById("music-img");
   if (e.target.checked) {
-    for (let m of music) {
+    for (let m of musics) {
       m.muted = false;
     }
-    //set image to unmuted
     img.setAttribute("src", "assets/music-normal.svg");
   } else {
-    for (let m of music) {
+    for (let m of musics) {
       m.muted = true;
     }
-    // set image to muted
     img.setAttribute("src", "assets/music-muted.svg");
   }
 });
@@ -56,10 +71,9 @@ sfxVol.addEventListener("click", (e) => {
 });
 
 addSquareListeners();
-initPrompt();
+initGame();
 
-function initPrompt() {
-  // music[0].play();
+function initGame() {
   if (p1turn) {
     textPromptTimer = typewriterFx(p1TurnPrompt);
   } else textPromptTimer = typewriterFx(p2TurnPrompt);
@@ -97,9 +111,17 @@ function showTextPrompt() {
   tp[0].classList.remove("hide");
 }
 
+function showNextMsg() {}
+
 function typewriterFx(str) {
   showTextPrompt();
   // wowowow this closure works surprisingly well.. TOO well
+  if (chain) {
+    str += "  ᐁ";
+    tp[0].addEventListener("click", showNextMsg);
+  } else {
+    tp[0].removeEventListener("click", showNextMsg);
+  }
   let c = 0;
   console.log(c);
   function run(str) {
@@ -142,6 +164,11 @@ function viewMove(n) {
       }
     }
   }
+  if (n == moveHistory.length) {
+    addWinHighlight();
+  } else {
+    removeWinHighlight();
+  }
 }
 
 let resetBtn = document.querySelector(".reset");
@@ -152,6 +179,7 @@ resetBtn.addEventListener("click", (e) => {
     square.classList.remove("p2-hover");
     square.classList.remove("p2-marker");
     square.classList.remove("no-ptr-events");
+    square.setAttribute("winner", "false");
     moveHistory = [];
 
     hideBtns();
@@ -201,17 +229,33 @@ function addSquareListeners() {
 
       this.classList.add("no-ptr-events");
 
-      if (findWin() != "") {
+      let winner = findWin();
+      if (winner != "" && winner != "draw") {
         showBtns();
         removePtrEvents();
-        showWinner(); // parse the winner + win location (or draw)
+        showWinner(winner); // parse the winner + win location (or draw)
 
+        playMusic("victory-music");
         clearInterval(textPromptTimer);
-        textPromptTimer = typewriterFx(p1TurnPrompt);
+        textPromptTimer = typewriterFx(
+          `Congratulations, ${winner.charAt(0)}!`,
+          true
+        );
       }
       moveN++;
     });
   }
+}
+
+function playMusic(musicId) {
+  for (let m of musics) {
+    // stop all first
+    m.pause();
+    m.currentTime = 0;
+  }
+  let selection = document.getElementById(musicId);
+  selection.play();
+  selection.loop = true;
 }
 function showBtns() {
   prevBtn.classList.remove("hide");
@@ -225,26 +269,33 @@ function hideBtns() {
   firstMoveBtn.classList.add("hide");
   lastMoveBtn.classList.add("hide");
 }
-function showWinner() {
-  // findWin returns: rowN | colN | main | reverse | draw
-  let len = moveHistory.length;
-  let win = findWin();
-  let result = "";
-  result =
-    win.substring(0, 3) == "row"
-      ? moveHistory[len - 1][+win.substring(3, 4)][0]
-      : win.substring(0, 3) == "col"
-      ? moveHistory[len - 1][0][+win.substring(3, 4)]
-      : win == "main"
-      ? moveHistory[len - 1][1][1]
-      : win == "draw"
-      ? moveHistory[len - 1][1][1]
-      : win == "reverse"
-      ? moveHistory[len - 1][1][1]
-      : `no winner?`;
-  console.log(`${win} ${result}`);
-  return result;
+
+function addWinHighlight() {
+  for (let square of squares) {
+    if (square.getAttribute("winner") == "true") {
+      if (!square.classList.contains("win")) {
+        square.classList.add("win");
+      }
+    }
+  }
 }
+
+function removeWinHighlight() {
+  for (let square of squares) {
+    square.classList.remove("win");
+  }
+}
+function showWinner(w) {
+  // findWin returns: *letter* *pair1* *pai r2* *pair3*
+  // highlights winning squares
+  let idxs = [w.substring(2, 4), w.substring(5, 7), w.substring(8, 10)];
+
+  for (let idx of idxs) {
+    document.getElementById(`s${idx}`).setAttribute("winner", "true");
+  }
+  addWinHighlight();
+}
+
 function removePtrEvents() {
   for (let square of squares) {
     if (!square.classList.contains("no-ptr-events")) {
@@ -282,7 +333,7 @@ function findWin() {
     // check rows
     if (arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2] && arr[i][0] != "") {
       console.log(`Winner on row ${i}!`);
-      return `row${i}`;
+      return `${arr[i][0]} ${i}0 ${i}1 ${i}2`;
     }
   }
 
@@ -290,18 +341,18 @@ function findWin() {
     // check cols
     if (arr[0][i] == arr[1][i] && arr[1][i] == arr[2][i] && arr[0][i] != "") {
       console.log(`Winner on column ${i}!`);
-      return `col${i}`;
+      return `${arr[0][i]} 0${i} 1${i} 2${i}`;
     }
   }
 
   if (arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[0][0] != "") {
     console.log(`Winner on main diag!`);
-    return `main`;
+    return `${arr[1][1]} 00 11 22`;
   }
 
   if (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0] && arr[0][2] != "") {
     console.log(`Winner on reverse diag!`);
-    return `reverse`;
+    return `${arr[1][1]} 02 11 20`;
   }
 
   outer: for (let i = 0; i < len; i++) {
